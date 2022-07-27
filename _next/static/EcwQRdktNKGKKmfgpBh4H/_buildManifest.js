b0ac478f8d37086f6672798bda5d53f07fe5ff93
grzracz <grz.raczek@gmail.com>
2022-07-27 21:42:24 +0200

    updated files

--- _next/static/EcwQRdktNKGKKmfgpBh4H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/545f34e4-fb18ed70d13cf611.js","static/chunks/492-044756063585acd1.js","static/chunks/pages/index-2c859576ce92540d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
